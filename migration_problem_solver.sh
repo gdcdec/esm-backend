2e6f3b84c97e3c6e80a50d927a613333b06cb502
WSOUL4 <mitrinov4124@gmail.com>
2026-03-04 00:33:42 +0400

    cleanup - Скрипт для починки проблемы миграций в новом пк при обновлении

---
+++ migration_problem_solver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker-compose down
+sudo rm -rf api/migrations/
+sudo mkdir -p api/migrations
+sudo touch api/migrations/__init__.py
+docker-compose down -v
+docker-compose up -d
+docker-compose exec django python manage.py makemigrations
+
+if [ "$1" == "--fake" ]; then
+    echo "Помечаем миграции как выполненные..."
+    docker-compose exec django python manage.py migrate --fake
+else
+    echo "Применяем миграции реально..."
+    docker-compose exec django python manage.py migrate
+fi
+
+docker-compose exec django python manage.py createsuperuser
