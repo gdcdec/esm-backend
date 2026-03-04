2122020ce8da472598d4c4aebe39a824cc9f5aa6
Dobby <dobby@example.xyz>
2026-03-04 07:29:40 +0000

    fix: Fixes of 'run_compose.sh' and 'deploy.yml'. Migration performs fine

--- run_compose.sh
+++ run_compose.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 
 rm -rf api/migrations/* pgadmin_data/ postgres_data/
+touch api/migrations/__init__.py
 
 docker compose up -d --build
-docker compose exec django python manage.py makemigrations api
+docker compose exec django python manage.py makemigrations
+docker compose exec django python manage.py migrate --fake
