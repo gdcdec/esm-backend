c2755a935dbe4a6335146bcd6c8095ec0d6cac67
Dobby <dobby@example.xyz>
2026-03-09 08:31:44 +0000

    fix: minor fixes of deployment scripts and CI/CD
    - Now one should execute 'run_compose.sh' without sudo
    - Small fixes of 'docker-compose.yml'

--- run_compose.sh
+++ run_compose.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -x
 
-rm -rf api/migrations/* pgadmin_data/ postgres_data/
+sudo rm -rf api/migrations/* pgadmin_data/ postgres_data/
 touch api/migrations/__init__.py
 
-docker compose up -d --build
-docker compose exec django python manage.py makemigrations
-docker compose exec django python manage.py migrate --fake
+UID=`id -u` GID=`id -g` docker compose up -d --build
+
+docker compose exec django python manage.py makemigrations api
+docker compose exec django python manage.py migrate
