75fdcbc576337a6b9df714cde4f90b8ba5c4da2d
WSOUL4 <mitrinov4124@gmail.com>
2026-03-09 16:14:00 +0400

    >(

--- run_compose.sh
+++ run_compose.sh
@@ -3,7 +3,7 @@
 #sudo rm -rf api/migrations/* pgadmin_data/ postgres_data/
 #touch api/migrations/__init__.py
 
-CURRENT_UID=`id -u` CURRENT_GID=`id -g` docker compose up -d --build
+MY_UID=`id -u` MY_GID=`id -g` docker compose up -d --build
 
 #docker compose exec django python manage.py makemigrations api --merge
 #docker compose exec django python manage.py migrate
